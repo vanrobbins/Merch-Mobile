7caaed51e29a9cfced2fc10eebe1a813f6f7a560
Van Robbins <van@example.com>
2026-04-13 15:49:42 -0400

    feat: add SignUpNotifier

---
+++ lib/features/auth/signup_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signup_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signUpNotifierHash() => r'4d0c7b83142f43129a9ae13b065398b0b6b1f9b0';
+
+/// See also [SignUpNotifier].
+@ProviderFor(SignUpNotifier)
+final signUpNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<SignUpNotifier, void>.internal(
+  SignUpNotifier.new,
+  name: r'signUpNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signUpNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SignUpNotifier = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
